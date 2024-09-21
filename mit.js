3030fd4944318c6daa2eeb5cd387ec2bcb487e4d
PARTHEESHWARAN P <dwaranp@gmail.com>
2024-09-21 09:34:52 +0530

    first update

--- mit.js
+++ mit.js
@@ -1,6 +1,6 @@
 var mit = {
-    city: 'Cambridge',
-    colors: ['pink', 'red'],
+    city: 'Theni',
+    colors: ['blue', 'red'],
     mascot: "robot",
     founded: 1900,
     motto: 'Carpe diem',
